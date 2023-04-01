bc3f07b55690df13e09dc1c71b47dfa2d8f0debe
Mohamed Elkerm <mohamedelkerm584@gmail.com>
2023-04-01 14:52:43 +0200

    add endPoints for place , category and city model

--- lib/helper/end_points.dart
+++ lib/helper/end_points.dart
@@ -5,5 +5,9 @@ class AppEndPoints{
   static const String signUp = '${baseUrl}users/signup/';
   static const String login = '${baseUrl}users/singin';
 
+  static const String getCategory = '${baseUrl}places/category/';
+  static const String getPlaces = '${baseUrl}places//';
+  static const String getCities = '${baseUrl}places/city/';
+
 
 }
