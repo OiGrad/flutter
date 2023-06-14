8ced14b1bb3c5645aa4487ed97ef7f000654ee79
hosam95 <hosamsaeid95@gmail.com>
2023-06-14 23:34:56 +0300

    integrate the favorites screen with backend

--- lib/modules/navigation_bar_modules/favorites/favorites_cubit.dart
+++ lib/modules/navigation_bar_modules/favorites/favorites_cubit.dart
@@ -1,8 +1,34 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+
+import '../../../helper/end_points.dart';
+import '../../../helper/remote/dio_helper.dart';
+import '../../../models/place_model.dart';
+import '../../widgets/snackbar_widget.dart';
 
 part 'favorites_state.dart';
 
 class FavoritesCubit extends Cubit<FavoritesState> {
   FavoritesCubit() : super(FavoritesInitial());
+
+  List<Place> places = [];
+
+  void getPlaces({required BuildContext context}) async {
+    emit(GetFavoritPlacesLoading());
+
+    await DioHelper.getData(
+      url: AppEndPoints.getFavPlaces,
+    ).then((value) {
+      //TODO: add the places to the placesList.
+      value.data['results'][0]['place'].forEach((e) {
+        places.add(Place.fromJson(e));
+      });
+      emit(GetFavoritPlacesSuccess());
+    }).catchError((err) {
+      showSnackBar(
+          context: context, text: 'Error in get Places', clr: Colors.red);
+      emit(GetFavoritPlacesError());
+    });
+  }
 }
--- lib/modules/navigation_bar_modules/favorites/favorites_screen.dart
+++ lib/modules/navigation_bar_modules/favorites/favorites_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kemet/core/colors.dart';
 import 'package:kemet/core/strings.dart';
 import 'package:kemet/helper/shimmer/card_shimmer.dart';
+import 'package:kemet/modules/navigation_bar_modules/favorites/favorites_cubit.dart';
 import 'package:kemet/modules/widgets/home_screen_widgets.dart';
 import 'package:kemet/core/media_query_values.dart';
 
@@ -12,33 +14,62 @@ class FavoritesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                welcomeWidget(context),
-                textWidget(context, AppStringsInArabic.favoritePlacesForYou,
-                    25.0, AppColors.black),
-                GridView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: 6,
-                  gridDelegate:
-                  SliverGridDelegateWithFixedCrossAxisCount(
-                    mainAxisExtent: MediaQueryValues(context).height*1/4,
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 20,
-                    crossAxisSpacing: MediaQueryValues(context).width*1/30,
+        child: BlocProvider(
+          create: (context) => FavoritesCubit()..getPlaces(context: context),
+          child: BlocConsumer<FavoritesCubit, FavoritesState>(
+            listener: (context, state) {},
+            builder: (context, state) {
+              var myBloc = BlocProvider.of<FavoritesCubit>(context);
+
+              if (state is GetFavoritPlacesError ||
+                  (state is GetFavoritPlacesSuccess && myBloc.places.isEmpty)) {
+                return Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Image.asset('assets/images/favorites_empty.png'),
+                );
+              }
+              return SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      welcomeWidget(context),
+                      textWidget(
+                          context,
+                          AppStringsInArabic.favoritePlacesForYou,
+                          25.0,
+                          AppColors.black),
+                      GridView.builder(
+                        physics: const BouncingScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: myBloc.places.length == 0
+                            ? 4
+                            : myBloc.places.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          mainAxisExtent:
+                              MediaQueryValues(context).height * 1 / 4,
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 20,
+                          crossAxisSpacing:
+                              MediaQueryValues(context).width * 1 / 30,
+                        ),
+                        itemBuilder: (context, item) {
+                          // return cardOfPlace(context);
+                          if (state is GetFavoritPlacesLoading) {
+                            return CardShimmer();
+                          }
+
+                          if (state is GetFavoritPlacesSuccess) {
+                            return cardOfPlace(context, myBloc.places[item]);
+                          }
+                        },
+                      ),
+                    ],
                   ),
-                  itemBuilder: (context, item) {
-                    // return cardOfPlace(context);
-                    return CardShimmer();
-                  },
                 ),
-              ],
-            ),
+              );
+            },
           ),
         ),
       ),
