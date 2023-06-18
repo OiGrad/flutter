55132136e6d0fd9065e28f2a13cd98c9705c3c66
hosam95 <hosamsaeid95@gmail.com>
2023-06-18 08:34:41 +0300

    implement post within the user-screen

--- lib/modules/navigation_bar_modules/user/user_screen_state.dart
+++ lib/modules/navigation_bar_modules/user/user_screen_state.dart
@@ -4,3 +4,9 @@ part of 'user_screen_cubit.dart';
 abstract class UserScreenState {}
 
 class UserScreenInitial extends UserScreenState {}
+
+class GetPostsLoading extends UserScreenState {}
+
+class GetPostsSuccess extends UserScreenState {}
+
+class GetPostsError extends UserScreenState {}
